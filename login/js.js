229b5729abe31754b8f04b2192d9d4ea97bf42a7
Srikantha_Arththika <arththika04@gmail.com>
2025-12-10 10:53:57 +0530

    login js

---
+++ login/js.js
@@ -0,0 +1,15 @@
+const password = document.getElementById("password");
+const togglePassword = document.getElementById("togglePassword");
+
+togglePassword.addEventListener("click", () => {
+    // Toggle type (password <-> text)
+    const currentType = password.getAttribute("type");
+    password.setAttribute(
+        "type", 
+        currentType === "password" ? "text" : "password"
+    );
+
+    // Change eye icon text for fun
+    togglePassword.textContent = 
+        currentType === "password" ? "🙈" : "👁️";
+});
